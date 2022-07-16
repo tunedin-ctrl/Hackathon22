240cb2031a91b56a697b2fd13e9e576c8f8598da
David Zhou <zhouyi021022@gmail.com>
2022-07-16 19:04:57 +1000

    Add router for search, and return type

--- backend/analysis.js
+++ backend/analysis.js
@@ -1,5 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 
+const fileName = "data.json";
+const dataFile = path.resolve(process.cwd()) + `\\${fileName}`;
 
-export function analysisSearchResult() {
+const result = JSON.parse(fs.readFileSync(fileName, 'utf8'));
 
+parseSearchResults(result);
+
+function parseSearchResults(result) {
+    let searchWeb = result.organic_results;
+    searchWeb = searchWeb.map(web => {
+        return {
+            title : web.title,
+            link : web.link,
+            snippet : web.snippet,
+            rank : web.position
+        }
+    });
+    searchWeb = searchWeb.slice(0, 4);
+    console.log(searchWeb);
 }
--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,62 @@
+
 const fs = require('fs');
 const path = require('path');
 const fileName = "data.json";
-console.log(path.resolve(process.cwd()) + `\\${fileName}`);
-//fs.writeFileSync(path.resolve(process.cwd()) + `\\${fileName}`, JSON.stringify({abc:"dsa"}));
+const dataFile = path.resolve(process.cwd()) + `\\${fileName}`;
 
 
 
-
-const SerpApi = require('google-search-results-nodejs')
-const search = new SerpApi.GoogleSearch("398b46ced9ed6a813762b6c29ce6d5593ed51c080a5e95273453978288232951")
-search.json({
-  q: "JavaScript Throw", 
-  location: "Sydney, New South Wales, Australia",
-	safe : "active",
-	num : "10",
-	google_domain: "google.com",
-}, (result) => {
-	fs.writeFileSync(path.resolve(process.cwd()) + `\\${fileName}`, JSON.stringify(result));
-  console.log(result);
+app.use('*', express.json());
+// start server
+app.listen(3000, () => {
+	console.log(`⚡️ Server listening on port 3000`);
 });
+
+
+
+
+const express = require('express');
+app.post('/', function (req, res) {
+	const { searchString } = req.body;
+
+	const SerpApi = require('google-search-results-nodejs')
+	const search = new SerpApi.GoogleSearch("398b46ced9ed6a813762b6c29ce6d5593ed51c080a5e95273453978288232951")
+	search.json({
+		q: searchString, 
+		location: "Sydney, New South Wales, Australia",
+		safe : "active",
+		num : "10",
+		google_domain: "google.com",
+	}, (result) => {
+		/**
+		 * Return first 5 results
+		 * @array {
+		 *   @link
+		 *   @title
+		 *   @snippet
+		 * }
+		 */
+		res.status(200).json(parseSearchResults(result));
+	});
+})
+
+
+
+
+function parseSearchResults(result) {
+    let searchWeb = result.organic_results;
+    searchWeb = searchWeb.map(web => {
+        return {
+            title : web.title,
+            link : web.link,
+            snippet : web.snippet,
+            rank : web.position
+        }
+    });
+    searchWeb = searchWeb.slice(0, 5);
+    console.log(searchWeb);
+}
+
+
 
 
