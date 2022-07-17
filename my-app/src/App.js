5ad5cfdc74bb58638e55829096beb2a6d0e2849f
tunein <z5371683@ad.unsw.edu.au>
2022-07-17 10:58:38 +1000

    Can communicate with backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 import {
-  Box,
-  Container,
-  Typography,
   Grid
 } from '@material-ui/core';
 import QueryCard from './Componenets/QueryCard'
@@ -17,30 +14,31 @@ function App() {
    */
    const [loading, setLoading] = React.useState(false);
    const [queries, setqueries] = React.useState([]);
+
    function handleDoubleClick () {
-     const query_string = window.getSelection.toString()
+     const searchString = window.getSelection.toString()
  
-     // setLoading(true);
-    setqueries("hi")
-    //  axios.post(`/search`, { query_string })
-    //    .then((response) => {
-    //      console.log(response);
-    //      setqueries(response.data)
-    //    })
-    //    .catch((err) => { console.log(err) })
-    //    .finally(() => setLoading(false));
+     setLoading(true);
+    // setqueries("hi")
+     axios.post(`http://localhost:3001/search/search`, { searchString })
+       .then((response) => {
+         console.log(response);
+         setqueries(response.data)
+       })
+       .catch((err) => { console.log(err) })
+       .finally(() => setLoading(false));
   }
    return(
-        
-          loading
-            ?<Grid container spacing={2}>
-            {queries.map(query => (
-                <QueryCard query={query} handleDoubleClick={handleDoubleClick}/>
-              ))}
-            </Grid> 
+  
+    loading
+      ?<Grid container spacing={2}>
+      {queries.map(query => (
+          <QueryCard query={query} handleDoubleClick={handleDoubleClick}/>
+        ))}
+      </Grid> 
+      : 
+      <h1>hi</h1>
 
-            : 
-            null
   );
 }
  
